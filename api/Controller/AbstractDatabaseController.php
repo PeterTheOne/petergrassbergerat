bfcdd272da90322906c727237aa8451410a14d7d
PeterTheOne <petertheone@gmail.com>
2013-02-10 22:25:43 +0100

    move database functions to AbstractDatabaseController.
    PageController now works with all parameters and order by parameters.

---
+++ api/Controller/AbstractDatabaseController.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @package api-framework
+ * @author Peter Grassberger <petertheone@gmail.com>
+ * @abstract
+ */
+
+include_once('../functions.inc.php');
+include_once("../config.inc.php");
+
+abstract class AbstractDatabaseController {
+
+    protected $table;
+    protected $allowedParameters = array();
+
+    /**
+     * @return mysqli
+     */
+    private function db_connect() {
+        /** @var $db_con mysqli */
+        $db_con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWD, DB_DBNAME);
+        //TODO: error handling..
+        if (!$db_con) {
+            die('Could not connect: ' . mysqli_error($db_con));
+        }
+        $db_con->set_charset("utf8");
+        return $db_con;
+    }
+
+    /**
+     * @param $db_con
+     * @param $result
+     * @return bool
+     */
+    private function db_hasErrors($db_con, $result) {
+        if(!$result){
+            if (PRINT_DB_ERRORS) {
+                echo "<p>error: " . mysqli_error($db_con) . "</p>";
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * GET method.
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function get($request) {
+        //TODO: allow for multiple ids in a comma sperated list
+
+        // filter unallowed parameters
+        $parameters = whitelist($request->parameters, $this->allowedParameters);
+
+        $db_con = $this->db_connect();
+        $query = "SELECT * FROM " . $this->table;
+        $query_where = array();
+        $query_order = "";
+
+        foreach ($parameters as $parameterName => $parameterValue) {
+            switch ($this->allowedParameters[$parameterName]) {
+                case 'int':
+                    $parameterValue = filter_var($parameterValue, FILTER_VALIDATE_INT);
+                    $query_where[] = "$parameterName = $parameterValue";
+                    break;
+                case 'string':
+                case 'timestamp':
+                    // TODO: sanitize string
+                    //$parameterValue = filter_var($parameterValue, FILTER_SANITIZE_STRING);
+                    $parameterValue = mysqli_real_escape_string($db_con, $parameterValue);
+                    $query_where[] = "$parameterName = '$parameterValue'";
+                    break;
+            }
+        }
+
+        if (count($query_where) != 0) {
+            $query .= ' WHERE ' . implode(' AND ', $query_where);
+        }
+
+        if (isset($request->parameters['orderby'])) {
+            $parameterValue = filter_var($request->parameters['orderby'], FILTER_SANITIZE_STRING);
+            $parameterValue = mysqli_real_escape_string($db_con, $parameterValue);
+
+            $orderArray = explode(',', $parameterValue);
+
+            $tempArray = array();
+            foreach ($orderArray as $key => $value) {
+                $value = endswithCrop($value, ' ASC');
+                $value = endswithCrop($value, ' DSC');
+                if (isset($this->allowedParameters[$value])) {
+                    $tempArray[$key] = $value;
+                }
+            }
+            $orderArray = $tempArray;
+
+            $query_order = implode(', ', $orderArray);
+        }
+
+        if (!empty($query_order)) {
+            $query .= ' ORDER BY ' . $query_order;
+        }
+
+        $result = mysqli_query($db_con, $query);
+        if ($this->db_hasErrors($db_con, $result)) {
+            return false;
+        }
+        $resultArray = array();
+        while($column = mysqli_fetch_array($result)) {
+            $resultArray[] = $column;
+        }
+        return $resultArray;
+    }
+}
--- api/Controller/PageController.php
+++ api/Controller/PageController.php
@@ -3,85 +3,21 @@
  * page controller.
  *
  * @package api-framework
- * @author Martin Bean <martin@martinbean.co.uk>
+ * @author Peter Grassberger <petertheone@gmail.com>
  */
 
-include_once("Framework/database.inc.php");
 
-class PageController {
+class PageController extends AbstractDatabaseController {
 
-    /**
-     * GET method.
-     *
-     * @param Request $request
-     * @return string
-     */
-    public function get($request) {
-        /*$articles = array(
-            array(
-                'name' => 'page 01',
-                'last_change' => 'date 01',
-                'title' => 'title 01',
-                'title_clean' => 'title_clean 01',
-                'lang' => 'lang 01',
-            ),
-            array(
-                'name' => 'page 02',
-                'last_change' => 'date 02',
-                'title' => 'title 02',
-                'title_clean' => 'title_clean 02',
-                'lang' => 'lang 02',
-            )
-        );*/
-
-        $db_con = db_connect();
-        $query = "SELECT * FROM pages";
-        $query_where = array();
-
-        //TODO: allow for multiple ids in a comma sperated list
-        if (isset($request->parameters['id'])) {
-            // get specific pages
-            $id = filter_var($request->parameters['id'], FILTER_VALIDATE_INT);
-            $id = mysqli_real_escape_string($db_con, $id);
-            $query_where[] = "id = $id";
-        }
-        if (isset($request->parameters['last_change'])) {
-            $last_change = $request->parameters['last_change'];
-            $last_change = mysqli_real_escape_string($db_con, $last_change);
-            $query_where[] = "last_change = '$last_change'";
-        }
-        if (isset($request->parameters['title'])) {
-            $title = $request->parameters['title'];
-            $title = mysqli_real_escape_string($db_con, $title);
-            $query_where[] = "title = '$title'";
-        }
-        if (isset($request->parameters['title_clean'])) {
-            $title_clean = $request->parameters['title_clean'];
-            $title_clean = mysqli_real_escape_string($db_con, $title_clean);
-            $query_where[] = "title_clean = '$title_clean'";
-        }
-        if (isset($request->parameters['lang'])) {
-            $lang = $request->parameters['lang'];
-            $lang = mysqli_real_escape_string($db_con, $lang);
-            $query_where[] = "lang = '$lang'";
-        }
-
-        if (count($query_where) != 0) {
-            $query .= ' WHERE ' . implode(' AND ', $query_where);
-        }
-
-        $result = mysqli_query($db_con, $query);
-        if (db_hasErrors($db_con, $result)) {
-            return false;
-        }
-        $resultArray = mysqli_fetch_array($result);
-        if ($resultArray == null) {
-            return array();
-        }
-        return $resultArray;
-
-        //return $articles[$id];
-        //return $articles;
+    public function  __construct() {
+        $this->table = 'pages';
+        $this->allowedParameters = array(
+            'id' => 'int',
+            'last_change' => 'timestamp',
+            'title' => 'string',
+            'title_clean' => 'string',
+            'lang' => 'string'
+        );
     }
 }
 ?>
