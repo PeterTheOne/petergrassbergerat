09eb24ece2ec7b6d73932e8ed5464c546e4ad5de
PeterTheOne <petertheone@gmail.com>
2013-02-10 22:24:12 +0100

    add some helper functions from https://github.com/meisterluk/unfuck.php/
    thanks meisterluk

--- functions.inc.php
+++ functions.inc.php
@@ -4,6 +4,107 @@ function sanitize($str) {
 	return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
+/**
+ * Return a subset of the parameter array with only keys of the whitelist.
+ * Only checks keys, not values.
+ *
+ * @author meisterluk
+ * @author Peter Grassberger <petertheone@gmail.com>
+ *
+ * @param $array array to select from
+ * @param $whitelist array of keys to select
+ * @return array a subset of array
+ */
+function whitelist($array, $whitelist) {
+    $new_array = array();
+    foreach ($array as $key => $value) {
+        if (isset($whitelist[$key]))
+            $new_array[$key] = $value;
+    }
+    return $new_array;
+}
+
+/**
+ *
+ * Return a subset of the parameter array with only keys not in blacklist.
+ * Only checks keys, not values.
+ *
+ * @author meisterluk
+ * @author Peter Grassberger <petertheone@gmail.com>
+ *
+ * @param $array array to select from
+ * @param $blacklist array of keys to throw away
+ * @return array a subset of array
+ */
+function blacklist($array, $blacklist) {
+    $new_array = array();
+    foreach ($array as $key => $value) {
+        if (!isset($blacklist[$key]))
+            $new_array[$key] = $value;
+    }
+    return $new_array;
+}
+
+/**
+ * Check whether parameter starts with a given substring or not.
+ *
+ * @author meisterluk
+ *
+ * @param $string string parameter to search in
+ * @param $substring substring needle to search for
+ * @return bool string starts with substring
+ */
+function startswith($string, $substring) {
+    return substr($string, 0, strlen($substring)) === $substring;
+}
+
+/**
+ * Check whether parameter ends with a given substring or not.
+ *
+ * @author meisterluk
+ *
+ * @param $string string parameter to search in
+ * @param $substring substring needle to search for
+ * @return bool string ends with substring
+ */
+function endswith($string, $substring) {
+    return substr($string, -strlen($substring)) === $substring;
+}
+
+/**
+ * Check whether parameter starts with a given substring or not.
+ *
+ * @author meisterluk
+ * @author Peter Grassberger <petertheone@gmail.com>
+ *
+ * @param $string string parameter to search in
+ * @param $substring string needle to search for
+ * @return bool string starts with substring
+ */
+function startswithCrop($string, $substring) {
+    if (startswith($string, $substring)) {
+        return substr($string, strlen($substring));
+    }
+    return $string;
+}
+
+/**
+ * Check whether parameter ends with a given substring or not.
+ *
+ * @author meisterluk
+ * @author Peter Grassberger <petertheone@gmail.com>
+ *
+ * @param $string string parameter to search in
+ * @param $substring string needle to search for
+ * @return bool string ends with substring
+ */
+function endswithCrop($string, $substring) {
+    if (endswith($string, $substring)) {
+        return substr($string, 0, -strlen($substring));
+    }
+    return $string;
+}
+
 function getLang() {
 	if(substr($_SERVER['HTTP_HOST'], -4) === '.com') {
 		$lang = 'en';
