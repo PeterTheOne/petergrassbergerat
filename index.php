fe3d7e6439472154321b72432d3837f2503a2707
PeterTheOne <petertheone@gmail.com>
2013-02-12 01:18:52 +0100

    restructure index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $url = getURL($lang);
 header("Content-language: $lang");
 
 // TODO: make url nonstatic
-$apiPath = 'http://localhost/petergrassbergerat/api';
+define('API_PATH', 'http://localhost/petergrassbergerat/api');
 
 // DISPLAY
 
@@ -29,61 +29,90 @@ $translateURL = buildTranslateURL($lang, $site, $subsite);
 $smarty->assign('translateURL', $translateURL);
 
 // display site
-if ($site === '404') {
-	header('HTTP/1.0 404 Not Found');
-	$smarty->display('404.tpl');
-} else if ($site === 'portfolio' && $subsite === '') {
-    // get portfolio overview:
+switch ($site) {
+    case '404':
+        display404($smarty);
+        break;
+
+    case 'portfolio':
+        if (empty($subsite)) {
+            displayPortfolioOverview($smarty, $lang);
+        } else {
+            displayProjectPage($smarty, $subsite, $lang);
+        }
+        break;
+
+    default:
+        if (empty($subsite)) {
+            displayPage($smarty, $site, $subsite, $lang);
+        } else {
+            display404($smarty);
+        }
+        break;
+}
+
+function displayPage($smarty, $site, $subsite, $lang) {
+    $parameters = array(
+        'lang' => $lang,
+        'title_clean' => $site
+    );
+    $result = getJsonFromUrl(API_PATH . '/page', $parameters);
+    if (isset($result[0]['downloadlink']) &&
+            $result[0]['downloadlink'] != '') {
+        $smarty->assign('downloadLink', $result[0]['downloadlink']);
+    }
+    if ($result) {
+        $smarty->assign('title', $result[0]['title']);
+        $smarty->assign('content', $result[0]['content']);
+        $smarty->display('page.tpl');
+    } else {
+        display404($smarty);
+    }
+}
+
+function displayPortfolioOverview($smarty, $lang) {
     $parameters = array(
         'lang' => $lang,
         'wip' => 1,
         'orderby' => 'wip DESC,year DESC,title'
     );
-    $wipProjectList = getJsonFromUrl($apiPath . '/project', $parameters);
+    $wipProjectList = getJsonFromUrl(API_PATH . '/project', $parameters);
 
     $parameters = array(
         'lang' => $lang,
         'wip' => 0,
         'orderby' => 'year DESC,title'
     );
-    $yearsProjectList = getJsonFromUrl($apiPath . '/project', $parameters);
+    $yearsProjectList = getJsonFromUrl(API_PATH . '/project', $parameters);
 
-	if (count($wipProjectList) + count($yearsProjectList) != 0 ) {
+    if (count($wipProjectList) + count($yearsProjectList) != 0 ) {
         $smarty->assign('wipProjectList', $wipProjectList);
         $smarty->assign('yearsProjectList', $yearsProjectList);
-		$smarty->display('portfolio.tpl');
-	} else {
-		header('HTTP/1.0 404 Not Found');
-		$smarty->display('404.tpl');
-	}
-} else {
-	if ($subsite === '') {
-        // get page
-        $parameters = array(
-            'lang' => $lang,
-            'title_clean' => $site
-        );
-        $result = getJsonFromUrl($apiPath . '/page', $parameters);
-        //print_r($result);
-		if (isset($result[0]['downloadlink']) &&
-				$result[0]['downloadlink'] != '') {
-			$smarty->assign('downloadLink', $result[0]['downloadlink']);
-		}
-	} else if ($site === 'portfolio') {
-        $parameters = array(
-            'lang' => $lang,
-            'title_clean' => $subsite
-        );
-        $result = getJsonFromUrl($apiPath . '/project', $parameters);
-	}
-	if ($result) {
-		$smarty->assign('title', $result[0]['title']);
-		$smarty->assign('content', $result[0]['content']);
-		$smarty->display('page.tpl');
-	} else {
-		header('HTTP/1.0 404 Not Found');
-		$smarty->display('404.tpl');
-	}
+        $smarty->display('portfolio.tpl');
+    } else {
+        display404($smarty);
+    }
+}
+
+function displayProjectPage($smarty, $subsite, $lang) {
+    $parameters = array(
+        'lang' => $lang,
+        'title_clean' => $subsite
+    );
+    $result = getJsonFromUrl(API_PATH . '/project', $parameters);
+
+    if ($result) {
+        $smarty->assign('title', $result[0]['title']);
+        $smarty->assign('content', $result[0]['content']);
+        $smarty->display('page.tpl');
+    } else {
+        display404($smarty);
+    }
+}
+
+function display404($smarty) {
+    header('HTTP/1.0 404 Not Found');
+    $smarty->display('404.tpl');
 }
 
 ?>
