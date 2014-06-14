a60feb6625487bb3baaed77891081011770faa9e
PeterTheOne <petertheone@gmail.com>
2014-06-14 11:26:16 +0200

    add tabby

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,17 +4,6 @@ $(function() {
         $('.editPreview h1 a').html($(this).val());
     });
 
-    $('#title_clean').on('keyup change', function() {
-        var href = $('.editPreview h1 a').attr('href');
-        href = href.split('/').slice(0, -2).join('/');
-        href += '/' + $(this).val() + '/';
-        $('.editPreview h1 a').attr('href', href);
-    });
-
-    $('#content').on('keyup change', function() {
-        $('.editPreview div').html($(this).val());
-    });
-
     if ($('#title_clean').val() === '') {
         $('#title').on('keyup change', function() {
             var title_clean = $(this).val();
@@ -24,4 +13,17 @@ $(function() {
             $('#title_clean').val(title_clean);
         });
     }
+
+    $('#title_clean').on('keyup change', function() {
+        var href = $('.editPreview h1 a').attr('href');
+        href = href.split('/').slice(0, -2).join('/');
+        href += '/' + $(this).val() + '/';
+        $('.editPreview h1 a').attr('href', href);
+    });
+
+    $('#content').tabby({tabString: '    '});
+
+    $('#content').on('keyup change', function() {
+        $('.editPreview div').html($(this).val());
+    });
 });
