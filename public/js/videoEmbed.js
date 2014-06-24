aec643b6cc505682b4331651b50509cbd89bede9
PeterTheOne <petertheone@gmail.com>
2014-06-24 13:11:12 +0200

    video embed: add cursor pointer and video autoplay on preview image click

--- public/js/videoEmbed.js
+++ public/js/videoEmbed.js
@@ -2,7 +2,7 @@ var videoEmbed = function() {
     $('div.videoEmbed').off('click').on('click', function() {
         $(this).find('img').hide();
         $(this).html(
-            '<iframe src="' + $(this).data('video-url') + '?title=0&byline=0&portrait=0" width="100%" height="100%" frameborder="0" webkitAllowFullScreen allowFullScreen></iframe>'
+            '<iframe src="' + $(this).data('video-url') + '?title=0&byline=0&portrait=0&autoplay=1" width="100%" height="100%" frameborder="0" webkitAllowFullScreen allowFullScreen></iframe>'
         );
     });
 };
